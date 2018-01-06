c0d8aec8891eb596a0b70da8c780600d14ddf199
Alex Dytrych <alex@dytry.ch>
2018-01-06 20:12:44 +0000

    remove junk from start script

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,4 @@
 document.write('Hello, world!');
 
-new Promise();
-new Map();
-
-console.log('hello world'.padStart(50));
-
 // auto-refresh
 if (module.hot) module.hot.dispose(() => window.location.reload());
