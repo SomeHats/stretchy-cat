5a53d3fddad38f6a29d6354f401153068fbbc061
Alex Dytrych <alex@dytry.ch>
2018-01-06 20:07:51 +0000

    initial commit

---
+++ src/scripts/index.js
@@ -0,0 +1,9 @@
+document.write('Hello, world!');
+
+new Promise();
+new Map();
+
+console.log('hello world'.padStart(50));
+
+// auto-refresh
+if (module.hot) module.hot.dispose(() => window.location.reload());
