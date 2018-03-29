7e055daefcd75c257e86757daf0f4f0a030c3b34
✨ Alex ✨ <alex@dytry.ch>
2018-03-29 21:13:00 +0100

    integrated flow

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,6 @@
+// @flow
 document.write('Hello, world!');
 
-// auto-refresh
+// auto-refresh in dev mode
+// $FlowFixMe - this isn't included in flow's module typedef
 if (module.hot) module.hot.dispose(() => window.location.reload());
